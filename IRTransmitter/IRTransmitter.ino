25859f465220d37b14d0cd44ab9e3a04fad023b5
JuanEspinosa <juancamilo100@hotmail.com>
2016-02-29 22:35:36 -0500

    Tested

--- IRTransmitter/IRTransmitter.ino
+++ IRTransmitter/IRTransmitter.ino
@@ -29,8 +29,7 @@
 IRsend irsend;
 
 int modulatingFrequencyKhz = 38;
-int ledPin = 53;
-int led2Pin = 12;
+
 Button incrementButton1(INCREMENT_BUTTON_ONE_PIN, PULLUP, INVERT, DEBOUNCE_MS);    //Declare the button
 Button decrementButton1(DECREMENT_BUTTON_ONE_PIN, PULLUP, INVERT, DEBOUNCE_MS);    //Declare the button
 Button incrementButton2(INCREMENT_BUTTON_TWO_PIN, PULLUP, INVERT, DEBOUNCE_MS);    //Declare the button
@@ -41,6 +40,20 @@ Button centerButton(CENTER_BUTTON_PIN, PULLUP, INVERT, DEBOUNCE_MS);    //Declar
 void MonitorMotorButton(Button button, const unsigned int buttonSignal, char signalSize)
 {
   if (button.wasPressed() || button.pressedFor(PRESS_AND_HOLD))
+  {
+    irsend.sendSony(buttonSignal, signalSize);
+    delay(80);
+  }
+  else if (button.wasReleased())
+  {
+    delay(50);
+    sleepNow();
+  }
+}
+
+void MonitorRelayButton(Button button, const unsigned int buttonSignal, char signalSize)
+{
+    if (button.wasPressed())
   {
     Serial.println("Button Pressed");
     irsend.sendSony(buttonSignal, signalSize);
@@ -61,7 +74,7 @@ void MonitorAllButtons()
   MonitorMotorButton(incrementButton2, INCREMENT_BUTTON_TWO_SIGNAL, SIGNAL_SIZE);
   MonitorMotorButton(decrementButton2, DECREMENT_BUTTON_TWO_SIGNAL, SIGNAL_SIZE);
   MonitorMotorButton(centerButton, CENTER_BUTTON_SIGNAL, SIGNAL_SIZE);
-  MonitorMotorButton(relayButton, RELAY_BUTTON_SIGNAL, SIGNAL_SIZE);
+  MonitorRelayButton(relayButton, RELAY_BUTTON_SIGNAL, SIGNAL_SIZE);
 }
 
 void UpdateAllButtonState()
@@ -76,27 +89,15 @@ void UpdateAllButtonState()
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
-  pinMode(led2Pin, OUTPUT);
-  Serial.begin(9600);
-  Serial.println("Entering Sleep Mode");
-  delay(50);
+  delay(20);
   sleepNow();
 }
 
 void loop() {
-
-
   UpdateAllButtonState();
   MonitorAllButtons();
 }
 /***********************************************/
-#define INCREMENT_BUTTON_ONE_PIN (2)
-#define DECREMENT_BUTTON_ONE_PIN (4)
-#define INCREMENT_BUTTON_TWO_PIN (5)
-#define DECREMENT_BUTTON_TWO_PIN (6)
-#define RELAY_BUTTON_PIN (7)
-#define CENTER_BUTTON_PIN (8)
 void sleepNow()
 {
   set_sleep_mode(SLEEP_MODE_PWR_SAVE);
@@ -117,10 +118,5 @@ void sleepNow()
 
 void pinInterrupt()
 {
-  uint8_t latest_interrupted_pin = PCintPort::arduinoPin;
-  Serial.println(latest_interrupted_pin);
-  Serial.println("Interrupt");
-  delay(20);
-
   detachInterrupt(0);
 }
